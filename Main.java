3054a713efbadf52d634c75a9eaae8753cc2ad60
ayaskhiri1 <aya.skhiri@eniso.u-sousse.tn>
2025-11-27 21:41:16 +0100

    Test trigger Jenkins

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour nedra!");
-  
+        System.out.println("Hello, Jenkins Trigger!"); 
     }
 }
-
